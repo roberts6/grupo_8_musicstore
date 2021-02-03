91ccc29cc586dd1fb277003b398bb5c4d08dc23e
roberts6 <roberts_oscar@hotmail.com>
2021-02-02 23:58:06 -0600

    ultimos cambios en la pagina de detalle del producto

--- music-store/app.js
+++ music-store/app.js
@@ -6,8 +6,12 @@ app.use('/static', express.static(__dirname + '/public'));
 
 const publicPath = path.resolve(__dirname, "./practicas-varias");
 
-app.listen(3506, () => console.log("Funcionando en el puerto 3506"));
+app.listen(3510, () => console.log("Funcionando en el puerto 3510"));
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "views/home.html")
+    )});
+
+app.get("/detalles", function(req, res){
+    res.sendFile(path.join(__dirname, "views/productDetail.html")
     )});
